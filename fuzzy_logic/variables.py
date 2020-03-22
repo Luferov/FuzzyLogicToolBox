d94ff434448f73270f9a1df59db5bc889df847b2
Luferov Victor <lyferov@yandex.ru>
2020-03-22 12:11:20 +0300

    feat: описана переменная сугено

--- fuzzy_logic/variables.py
+++ fuzzy_logic/variables.py
@@ -6,7 +6,8 @@ Fuzzy Variable
     - Sugeno
 """
 
-from typing import List
+from abc import ABC, abstractmethod
+from typing import List, Dict
 from .terms import Term
 
 
@@ -20,3 +21,61 @@ class FuzzyVariable:
         self.max_value: float = max_value
         self.terms: List[Term] = list(terms)
 
+    def term_by_name(self, name: str) -> Term or None:
+        """
+        Find term by name
+        :param name: name of term
+        :return: term
+        """
+        for term in self.terms:
+            if term.name == name:
+                return term
+
+
+class SugenoFunction(ABC):
+    """
+    Sugeno function interfaces
+    """
+    @property
+    @abstractmethod
+    def name(self) -> str:
+        ...
+
+    @abstractmethod
+    def evaluate(self, inputs: Dict[FuzzyVariable, float]) -> float:
+        ...
+
+
+class LinearSugenoFunction(SugenoFunction):
+
+    def __init__(self, name: str, coefficients: Dict[FuzzyVariable, float], const: float = .0):
+        self.__name: str = name
+        self.coefficients: Dict[FuzzyVariable, float] = coefficients
+        self.const: float = const
+
+    @property
+    def name(self) -> str:
+        return self.__name
+
+    def evaluate(self, inputs: Dict[FuzzyVariable, float]) -> float:
+        """
+        Calculate linear function
+        :param inputs: Values
+        :return: result of calculation
+        """
+        return self.const + sum([self.coefficients[variable] * value for variable, value in inputs.items()])
+
+
+class SugenoVariable:
+    """
+    Sugeno variable
+    """
+
+    def __init__(self, name: str, functions: List[SugenoFunction]):
+        self.name: str = name
+        self.functions: List[SugenoFunction] = functions
+
+    def function_by_name(self, name: str) -> SugenoFunction:
+        for function in self.functions:
+            if function.name == name:
+                return function
